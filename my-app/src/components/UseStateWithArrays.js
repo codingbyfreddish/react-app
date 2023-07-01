4c81837181c283ed7d5c82d0811c7f4520e867d9
Fredrik Gustafsson <freddish@hotmail.com>
2023-07-01 08:55:52 +0000

    add UseStateWithArrays.js /components

--- my-app/src/components/UseStateWithArrays.js
+++ my-app/src/components/UseStateWithArrays.js
@@ -1,9 +1,24 @@
 import React, {useState} from 'react'
 
 function UseStateWithArrays() {
+  const [nums, setNums] = useState([1,2,3]);
+  const addNums = () => {
+    setNums([...nums, nums.length +1]);
+  }
+  const removeNum = () => {
+    setNums(
+      nums.filter((item, idx) => {
+        return idx !== nums.length -1;
+      })
+    )
+  } 
   return (
     <div>
-        it works!
+        <button onClick={addNums}>Add item</button>
+        <button onClick={removeNum}>Remove item</button>
+        <ul>
+          {nums.map(num => <li key={num}>{num}</li>)}
+        </ul>
     </div>
   )
 }
