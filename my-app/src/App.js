c3497e6dcd0e3cdd910c238962392821817d832b
Fredrik Gustafsson <freddish@hotmail.com>
2023-07-01 09:57:50 +0000

    add UseEffectCounter.js /components

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-import UseStateWithObjects from './components/UseStateWithObjects';
+import UseEffectCounter from './components/UseEffectCounter';
+
 function App() {
   return (
     <div className="App">
-      <UseStateWithObjects />
+      <UseEffectCounter />
     </div>
   );
 }
