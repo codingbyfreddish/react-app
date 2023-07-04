39fa13dd7a85c98743836239110d285cef2a6613
Fredrik Gustafsson <freddish@hotmail.com>
2023-07-04 05:52:15 +0000

    add componentDidMount(), render posts HTTPRequests.js

--- my-app/src/components/HTTPRequests.js
+++ my-app/src/components/HTTPRequests.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import axios from 'axios';
 
 export class HTTPRequests extends Component {
     constructor(props) {
@@ -8,10 +9,33 @@ export class HTTPRequests extends Component {
          posts: []
       }
     }
+    componentDidMount() {
+      axios.get('https://jsonplaceholder.typicode.com/posts')
+      .then(response => {
+        console.log(response);
+        this.setState({
+          posts: response.data
+        })
+      })
+    }
   render() {
+    const posts = this.state.posts;
     return (
       <div>
             <h2>Posts:</h2>
+            {
+              posts.length ? (
+                  posts.map(post => (
+                    <div key={post.id}>
+                        <h2>{post.id}.{post.title}</h2>
+                        <h4> By User ID {post.userId}</h4>
+                        <p>{post.body}</p>
+                        <hr />
+                    </div>
+                  ))
+              ) : (
+                  <h4>Loading posts ...</h4>)
+            }
       </div>
     )
   }
