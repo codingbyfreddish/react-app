f99aeee09e1e2993d08c22acc3a043ce2867d7c0
Fredrik Gustafsson <freddish@hotmail.com>
2023-07-04 07:24:42 +0000

    add HTTPPost.js /components

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-import HTTPRequests from './components/HTTPRequests';
+import HTTPPost from './components/HTTPPost';
 
 function App() {
   return (
     <div className="App">
-      <HTTPRequests />
+      <HTTPPost />
     </div>
   );
 }
